2f61ac32fe177508d87087fcfab9143508b03cc0
ctiger <chb1828@naver.com>
2021-01-22 18:01:45 +0900

    divide authorized code

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 
@@ -5,16 +6,20 @@ from util import setEnvironment
 from . import models, serializers
 
 
+class IsAuthorized(BasePermission):
+    def has_permission(self, request, view):
+        auth = request.META.get("HTTP_AUTHENTICATION")
+        api_key = setEnvironment.get_secret("API_AUTH_KEY")
+        return auth == api_key
+
+
 class StockViewset(viewsets.ModelViewSet):
     queryset = models.Stock.objects.all()
     serializer_class = serializers.StockSerializer
+    permission_classes = [IsAuthorized]
 
     def create(self, request, *args, **kwargs):
         stock_data = request.data.get("data")
-        auth = request.META.get('HTTP_AUTHENTICATION')       # 헤더에서 값을 가져옴
-        api_key = setEnvironment.get_secret("API_AUTH_KEY")
-        if auth != api_key:  # secrets.json 에서 토큰값 비교
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = self.get_serializer(data=stock_data, many=True)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
