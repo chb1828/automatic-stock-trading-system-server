8ff36f0a2dd79dc9fa9177e10931d7bd9681c2a2
최현범 <chb1828@naver.com>
2021-01-24 21:33:11 +0900

    fix api url

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 from rest_framework import viewsets, status
@@ -6,19 +7,16 @@ from util import setEnvironment
 from . import models, serializers
 
 
-class IsAuthorized(BasePermission):
-    def has_permission(self, request, view):
-        auth = request.META.get("HTTP_AUTHENTICATION")
-        api_key = setEnvironment.get_secret("API_AUTH_KEY")
-        return auth == api_key
-
-
 class StockViewset(viewsets.ModelViewSet):
     queryset = models.Stock.objects.all()
     serializer_class = serializers.StockSerializer
-    permission_classes = [IsAuthorized]
 
-    def create(self, request, *args, **kwargs):
+    @action(detail=False, methods=['POST'], name='bulk save')
+    def bulk(self, request, *args, **kwargs):
+        auth = request.META.get('HTTP_AUTHENTICATION')
+        api_key = setEnvironment.get_secret("API_AUTH_KEY")
+        if auth != api_key:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         stock_data = request.data.get("data")
         serializer = self.get_serializer(data=stock_data, many=True)
         serializer.is_valid(raise_exception=True)
